8d0a890f10b3391c3258970b5a3cd92acc2ac64a
Justin Litchfield <litch@me.com>
2013-02-24 10:10:14 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectStalker::Application.config.secret_key_base = 'f14bbfa11d26685c5922917946bcedfaa022fa263f2d6e7b5979ecd448f500c72b6a4d3627c4bcd94b45c4bdc87e55fb7c2e9b50fd2c8ff490b0ed5025b11ae5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectStalker::Application.config.session_store :encrypted_cookie_store, key: '_project_stalker_session'
